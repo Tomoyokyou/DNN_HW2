c0d85c091a74542d890058cf4c2d91afe15733e8
jasonmhwu <j001001j001001@gmail.com>
2015-05-01 10:23:28 +0800

    cc

--- train.sh
+++ train.sh
@@ -1,15 +1,18 @@
-C=0.1
+C=0.01
 kernel_type=0
 order=1
 epsilon=0.05
-loss_type=1
+loss_type=0
 #=====================
 #TEST=/home/hui/model/u10.ark
-#TEST=/home/hui/model/test/test_gender_norm.svm
-TEST=/home/hui/model/train/train_gender_norm.svm
-MODEL=./model/test_${C}_${epsilon}.mdl
-OUTPUT=./model/${C}_${epsilon}_temp.seq
+TEST=/home/hui/model/test/test_gender_norm.svm
+TRAIN=/home/hui/model/train/train_gender_norm.svm
+#MODEL=./model/test_${C}_${epsilon}.mdl
+#MODEL=/home/larry/Documents/MLDS/DNN_HW2/model/test_1_0.05.mdl
+MODEL=./model/temp.mdl
+#OUTPUT=./model/${C}_${epsilon}_temp.seq
+OUTPUT=./model/temp.seq
 mkdir -p model
 mkdir -p log
-./svm_struct/svm_empty_learn.app -c ${C} -t ${kernel_type} -d ${order} -e ${epsilon} -l ${loss_type} ${TRAIN} ${MODEL} | tee ./log/${C}_${epsilon}.log
+#./svm_struct/svm_empty_learn.app -c ${C} -t ${kernel_type} -d ${order} -e ${epsilon} -l ${loss_type} ${TRAIN} ${MODEL} | tee ./log/${C}_${epsilon}.log
 ./svm_struct/svm_empty_classify.app ${TEST} ${MODEL} ${OUTPUT}
