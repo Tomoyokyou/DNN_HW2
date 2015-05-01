f6d3afe981fa8c45f5dd54b71b8d2ad8bb640635
Hui <hui811116@outlook.com>
2015-05-01 10:18:17 +0800

    hui local

--- train.sh
+++ train.sh
@@ -1,12 +1,14 @@
-C=0.01
+C=0.1
 kernel_type=0
 order=1
 epsilon=0.1
 loss_type=1
 #=====================
-#TEST=/home/hui/model/u10.ark
-TEST=/home/hui/model/test/test_gender_norm.svm
-TRAIN=/home/hui/model/train/train_gender_norm.svm
+TEST=/home/hui/model/u10.ark
+TRAIN=/home/hui/model/u10.ark
+#TEST=/home/hui/model/test/test_gender_norm.svm
+#TRAIN=/home/hui/model/train/train_gender_norm.svm
+
 MODEL=./model/test_${C}_${epsilon}.mdl
 OUTPUT=./model/${C}_${epsilon}.seq
 mkdir -p model
